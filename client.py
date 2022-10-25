a7560e3a6a8b205ac9807d4f853b81cf9c0fe08d
Eremeev D <den@den.ru>
2022-10-25 14:42:27 +0300

    Done 1-8 tasks

--- client.py
+++ client.py
@@ -1,16 +1,56 @@
 import socket
+import logging
 from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+stand_port = 9090
+stand_name = 'localhost'
+print("Введите имя хоста: ")
+hostname = input()
+if hostname == "":
+	hostname = stand_name
+print("Введите номер порта: ")
+port = int(input())
+if port == 0:
+	port = stand_port
+	
+sock.connect((hostname, port))
 
-data = sock.recv(1024)
+#Получение "приветственного" сообщения от сервера/запрос имени
 
+data = sock.recv(1024).decode()
+print(data)
+
+if data == "Введите Ваше имя: ":
+
+	name = input()
+	sock.send(name.encode())
+	print(sock.recv(1024).decode())
+	password = input()
+	sock.send(password.encode())
+	print(sock.recv(1024).decode())
+	
+else:
+
+	password = input()
+	sock.send(password.encode())
+	answ = sock.recv(1024).decode()
+	print(answ + '\n')
+
+
+answ = sock.recv(1024).decode()
+print(answ)
+
+#Отправка сообщений
+
+msg = ""
+while True:
+	msg = input()
+	if msg == "exit":
+		sock.send(("Клиент разорвал соединение \n").encode())
+		break
+	sock.send(msg.encode())
+	
 sock.close()
-
-print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,112 @@
 import socket
+import logging
+import csv
+import sys
+import base64
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+#Функция авторизации пользователей
+def auth (addr):
 
-msg = ''
+    ip = addr[0]
+    arr = {}
+
+    #Чтение списка пользователей из csv файла
+    with open('users.csv', 'r') as users:
+        reader = csv.reader(users)
+        for row in reader:
+            for i in range(len(row)):
+                cur_arr = row[i].split(";")
+                arr[cur_arr[0]] = [cur_arr[1],cur_arr[2]]
+
+    #Проверка пользователя, передача соответствующих сообщений 
+    if str(ip) in arr.keys():
+        conn.send(("Введите пароль: ").encode())
+        password = conn.recv(1024).decode()
+        if password == arr[str(ip)][1]:
+            hi_msg = "Добро пожаловать, " + str(arr[str(ip)][0])
+            conn.send(hi_msg.encode())
+
+    #При отсутствии пользователя в файле, вызов функции регистрации
+    else:
+        reg(ip, arr)
+
+#Функция регистрации пользователей        
+def reg(ip, arr):
+
+    conn.send(("Введите Ваше имя: ").encode())
+    name = conn.recv(1024).decode()
+    conn.send(("Придумайте пароль: ").encode())
+    password = conn.recv(1024).decode()
+    hi_msg = "Добро пожаловать, " + name 
+    conn.send(hi_msg.encode())
+    new_user = [str(ip), name, password]
+    with open('users.csv', 'w', newline='') as users:
+        writer = csv.writer(users, delimiter = ';')
+        writer.writerow(new_user)
+
+#Создание лог файла
+
+logger = logging.getLogger("logger")
+logger.setLevel(logging.INFO)
+log_handler = logging.FileHandler(filename = "info.log", encoding = "UTF-8")
+log_handler.setLevel(logging.INFO)
+logger.addHandler(log_handler)
+
+sock = socket.socket() 
+
+#Назначение порта для прослушивания
+
+stand_port = 9090
+print("Введите номер порта: ")
+inp_port = int(input())
+if inp_port == 0:
+    inp_port = stand_port
+
+port_ok = False
+while inp_port >=0 and inp_port <= 1024:
+    print("Данный порт занят, введите новое значение: ")
+    inp_port = int(input())
+
+port = inp_port
+
+#Автоподбор свободного порта
+
+con_res = False
+while con_res == False:
+    try:
+        sock.bind(('', port))
+        con_res = True
+    except OSError:
+        port = port+1
+
+if port != inp_port:
+    print("Порт: ", inp_port, "занят; Прослушивается порт: ", port)
+else:
+    print("Прослушивается порт: ", port)
+
+sock.listen(1)
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
 
-print(msg)
+    conn, addr = sock.accept()
+    logger.info(addr)
 
-conn.close()
+    #
+    auth(addr)
+            
+    conn.send("Введите сообщение; Для выхода введите exit: ".encode())
+    
+    #Прием сообщений
+    msg = ''
+
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        msg = data.decode()
+        print(msg)
+
+    
+
+    conn.close()
+      
